568df7e0fb8c5857a68ea559f3d1566d11d35f2a
Ranjan Mohan <ranjanmj@gmail.com>
2020-12-21 02:27:37 -0500

    Added a sample WordCounter
    Closes #1.
    Also added:
    - Support for converting a given exception class name string to an exception class.
    - Tests with 100% code coverage.

---
+++ src/main/java/parameterconverters/ExceptionClassConverter.java
@@ -0,0 +1,48 @@
+package parameterconverters;
+
+import java.util.Objects;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ArgumentConverter;
+
+/**
+ * Used to convert a given string into an exception class.
+ */
+public class ExceptionClassConverter implements ArgumentConverter {
+
+  /**
+   * Converts a given exception class string to the class object.
+   *
+   * @param expectedExceptionClassString String representation of the exception class.
+   * @return The exception class object corresponding to the input string.
+   * @throws ArgumentConversionException When an error occurs during conversion.
+   */
+  public static Class<? extends Exception> convertExceptionNameToClass(
+      String expectedExceptionClassString
+  ) throws ArgumentConversionException {
+    // Input validation
+    Objects.requireNonNull(expectedExceptionClassString);
+
+    return switch (expectedExceptionClassString) {
+      case "NullPointerException.class" -> NullPointerException.class;
+      case "IllegalArgumentException.class" -> IllegalArgumentException.class;
+      default -> throw new ArgumentConversionException(
+          "Unable to parse expected exception from input string: " + expectedExceptionClassString
+      );
+    };
+  }
+
+  /**
+   * Converts a given exception class string to the class object.
+   *
+   * @param expectedExceptionClassString String representation of the exception class.
+   * @param context The parameter context where the converted object will be used.
+   * @return The exception class object corresponding to the input string.
+   * @throws ArgumentConversionException When an error occurs during conversion.
+  */
+  @Override
+  public Object convert(Object expectedExceptionClassString, ParameterContext context)
+      throws ArgumentConversionException {
+    return convertExceptionNameToClass(expectedExceptionClassString.toString());
+  }
+}
---
+++ src/test/java/tests/WordCounterTest.java
@@ -0,0 +1,48 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
+import parameterconverters.ExceptionClassConverter;
+import wordspackage.WordCounter;
+
+/**
+ * Tests the WordCounter class.
+ */
+public class WordCounterTest {
+
+  /**
+   * Tests wordspackage.WordCounter::getWordCount() with invalid string inputs.
+   *
+   * @param str Invalid string input.
+   * @param expectedExceptionClass Exception expected to be thrown.
+   */
+  @ParameterizedTest
+  @CsvSource({", NullPointerException.class"})
+  public void testGetWordCountWithInvalidInput(
+      String str,
+      @ConvertWith(ExceptionClassConverter.class)
+          Class<? extends Exception> expectedExceptionClass) {
+    Assertions.assertThrows(expectedExceptionClass, () -> WordCounter.getWordCount(str));
+  }
+
+  /**
+   * Tests wordspackage.WordCounter::getWordCount() with invalid string inputs.
+   *
+   * @param str Invalid string input.
+   * @param expectedWordCount Expected word count.
+   */
+  @ParameterizedTest
+  @CsvSource({
+      "'', 0",
+      "a, 1",
+      "The little brown fox jumped over the lazy dog, 9",
+      "'The little brown fox\tjumped over the lazy dog', 9",
+      "'The little brown fox\rjumped over the lazy dog', 9",
+      "'The little brown fox\njumped over the lazy dog', 9",
+  })
+  public void testGetWordCount(String str, long expectedWordCount) {
+    Assertions.assertEquals(expectedWordCount, WordCounter.getWordCount(str));
+  }
+}
